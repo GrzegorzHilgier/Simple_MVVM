97d00ea829dcd212aef20427c3740e93a2da8038
Grzegorz Hilgier <g.hilgier@gmail.com>
2019-02-28 10:49:45 +0100

    Delete WhiteLines

--- ViewModels/ConvertWindowModel.cs
+++ ViewModels/ConvertWindowModel.cs
@@ -10,6 +10,17 @@ namespace Simple_MVVM.ViewModels
         private readonly TextConverter _textConverter = new TextConverter(s => s.ToUpper());
         private string _inputText;
         private readonly ObservableCollection<string> _history = new ObservableCollection<string>();
+        private void ConvertText()
+        {
+            if (string.IsNullOrWhiteSpace(InputText)) return;
+            AddToHistory(_textConverter.ConvertText(InputText));
+            InputText = string.Empty;
+        }
+        private void AddToHistory(string item)
+        {
+            if (!_history.Contains(item))
+                _history.Add(item);
+        }
 
         public string InputText
         {
@@ -20,28 +31,14 @@ namespace Simple_MVVM.ViewModels
                 RaisePropertyChangedEvent("InputText");
             }
         }
-
         public IEnumerable<string> History
         {
             get { return _history; }
         }
-
         public ICommand ConvertTextCommand
         {
             get { return new SimpleCommand(ConvertText); }
         }
 
-        private void ConvertText()
-        {
-            if (string.IsNullOrWhiteSpace(InputText)) return;
-            AddToHistory(_textConverter.ConvertText(InputText));
-            InputText = string.Empty;
-        }
-
-        private void AddToHistory(string item)
-        {
-            if (!_history.Contains(item))
-                _history.Add(item);
-        }
     }
 }
